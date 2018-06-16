bc60b779e38c7523a3d359858497e0cefa2aaace
Lenovo <dev1@LAPTOP-P5I6VDA3>
2018-06-16 19:21:19 +0530

    update::-->dev1

--- 01.GitHubFirstProject/src/com/bibhu/github/GitHubTestClass.java
+++ 01.GitHubFirstProject/src/com/bibhu/github/GitHubTestClass.java
@@ -3,12 +3,12 @@ package com.bibhu.github;
 public class GitHubTestClass {
 	
 	//create sum method and call it in main method
-	public int sumOf(int first, int second){
+	public static int sumOf(int first, int second){
 		
 		return first+second;
 	}
 	public static void main(String[] args) {
-		
+		System.out.println("The sum of given 2 number is "+sumOf(5,5));
 	}
 
 }
