952e5807466f3d223bb30c27151ed2b10118cd61
Lenovo <tl@LAPTOP-P5I6VDA3>
2018-06-16 18:51:17 +0530

    tl::--->tl commit

--- 01.GitHubFirstProject/src/com/bibhu/github/GitHubTestClass.java
+++ 01.GitHubFirstProject/src/com/bibhu/github/GitHubTestClass.java
@@ -3,5 +3,12 @@ package com.bibhu.github;
 public class GitHubTestClass {
 	
 	//create sum method and call it in main method
+	public int sumOf(int first, int second){
+		
+		return first+second;
+	}
+	public static void main(String[] args) {
+		
+	}
 
 }
